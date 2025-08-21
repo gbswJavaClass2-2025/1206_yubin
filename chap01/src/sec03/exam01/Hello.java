16a63d0fd9b9fba0c18e30171feee0245a219275
Yubin-0125 <yb45658006@gmail.com>
2025-08-21 14:37:59 +0900

    0821

---
+++ chap01/src/sec03/exam01/Hello.java
@@ -0,0 +1,18 @@
+package sec03.exam01;
+
+/**
+ * @author 유빈
+ */
+
+public class Hello {
+
+	/*
+	 * 범위주
+	 */
+	public static void main(String[] args) {
+		// 콘솔에 출력하
+		System.out.println("Hello, Java");
+
+	}
+
+}
